9d396b09f8d56c45d569fde4117048e3aea93fbd
Hazdryx <hazdryx@gmail.com>
2021-05-14 16:34:29 -0500

    Added the q function to do everything in one line.

--- src/Q.php
+++ src/Q.php
@@ -127,4 +127,19 @@
             return $result;
         }
     }
+
+    /**
+     * Creates and executes a Q object.
+     * 
+     * @author Christopher Bishop
+     * @param mysqli - The database to call the query on.
+     * @param string $query - An sql query with parameters.
+     * @param array $params - An array of parameters (default: []).
+     * @param bool $throwError - Whether to throw an error when it fails (default: true).
+     * @return QResult the result of the Q executing.
+     */
+    function q(mysqli $db, string $query, array $params = [], bool $throwError = true): QResult {
+        $q = new Q($query, $params);
+        return $q->execute($db);
+    }
 ?>
